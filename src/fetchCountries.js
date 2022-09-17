57ccc9de56fe23133ea9242dff46ac4ff12c2183
grant956 <68757554+Psihodel666@users.noreply.github.com>
2022-09-17 12:41:38 +0300

    add work

---
+++ src/fetchCountries.js
@@ -0,0 +1,14 @@
+
+import Notiflix from 'notiflix';
+export function fetchCountries(searchName) {
+  const BASE_URL = 'https://restcountries.com/v3.1';
+
+  return fetch(
+    `${BASE_URL}/name/${searchName}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(Notiflix.Notify.failure('Oops, there is no country with that name'))
+    }
+    return response.json();
+  });
+}
