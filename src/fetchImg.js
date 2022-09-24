35d74363c4271a2ee89d9f84e40379dee42b9250
grant956 <68757554+Psihodel666@users.noreply.github.com>
2022-09-24 15:34:04 +0300

    1

--- src/fetchImg.js
+++ src/fetchImg.js
@@ -1,17 +1,53 @@
-export async  function fetchImg(searchName) {
-  const axios = require('axios');
-  const BASE_URL = 'https://pixabay.com/api/';
-  const key = '30110883-082a3209dc2bc0d9dd6730db6';
+// export async  function fetchImg(searchName) {
+//   const axios = require('axios');
+//   const BASE_URL = 'https://pixabay.com/api/';
+//   const key = '30110883-082a3209dc2bc0d9dd6730db6';
   
-  let page = 1; 
-  return axios.get(
-    `${BASE_URL}?key=${key}&q=${searchName}&image_type=photo&orientation=horizontal&safesearch&per_page=40&page=${page}`
-  ).then(response => {
-    if(response.status !== 200){
-      throw new Error('Oops, there is no country with that name')
-    }
-    return response;
-  })
+//   let page = 1; 
+//   return axios.get(
+//     `${BASE_URL}?key=${key}&q=${searchName}&image_type=photo&orientation=horizontal&safesearch&per_page=40&page=${page}`
+//   ).then(response => {
+//     if(response.status !== 200){
+//       throw new Error('Oops, there is no country with that name')
+//     }
+//     return response;
+//   })
 
   
-}
+// }
+import axios from "axios";
+const BASE_URL = "https://pixabay.com/api/";
+
+export class searchQuery {
+   
+   static page = 1;
+   static image_type = "photo";
+   static key = '29904861-3a00656c8c471d49ecd8a16bd';
+   static query = '';
+   static orientation ='horizontal';
+   static safesearch = 'true';
+   static per_page = 40;
+   static maxPage = 13;
+
+   static async searchPictures(query = '') {
+    if(query.trim()) searchQuery.query = query;
+
+    
+   const config = {
+        params: {
+            key: searchQuery.key,
+            q: searchQuery.query,
+            image_type: searchQuery.image_type,
+            orientation: searchQuery.orientation,
+            safesearch: searchQuery.safesearch,
+            per_page: searchQuery.per_page,
+            page: searchQuery.page,
+            maxPage: searchQuery.maxPage,
+        }
+       }
+
+       const response = await axios.get(`${BASE_URL}`, config);
+       return response.data;
+   }
+
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
-import {fetchImg} from './fetchImg';
+import axios from "axios";
+import { searchQuery} from './fetchImg';
 var debounce = require('debounce');
 
 const refs = {
@@ -8,48 +9,41 @@ const refs = {
     input : document.querySelector('input'),
     button : document.querySelector('button'),
     conteinerImg : document.querySelector('.gallery'),
-    buttonPage : document.querySelector('.btn-page')
+    buttonPage : document.querySelector('.js-load-more')
     
 }
 
 
-
-
-
-
 refs.form.addEventListener('submit', searchImg)
+refs.buttonPage.addEventListener('click', onButtonClick)
 
-
-
-
-
-
-
-
-function searchImg(e){
+async function searchImg(e){
     e.preventDefault()
-    const inputValue = refs.input.value.trim();
-    refs.conteinerImg.innerHTML = '';
+    const query = e.target.elements.searchQuery.value.trim();
+    const response = await searchQuery.searchPictures(query);
     
-    fetchImg(inputValue)
+    const imgArr = response.hits;
 
-    .then((resp) => {
-        
-        const imgArr = resp.data.hits
-        if(imgArr.length <= 0 || inputValue.length === 0){
-            refs.conteinerImg.innerHTML = '';
+    try {
+      refs.conteinerImg.innerHTML = '';
+        if(imgArr.length === 0){
             return Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
-        }else{
+        }else if(!query){
+          Notiflix.Notify.info('Please, enter key word for search!');
+          return
+        }
+        else{
             refs.conteinerImg.insertAdjacentHTML('beforeend', markup(imgArr))
-            
+            refs.buttonPage.classList.remove('visually-hidden')
+            Notiflix.Notify.success(`Hooray! We found ${response.totalHits} images.`);
         }
         
         
-    })
+    }
    
-    .catch((error)=>{
+    catch(error){
         Notiflix.Notify.failure('error')
-    })
+    }
 }
 
 
@@ -102,3 +96,18 @@ function markup(arrImg){
 //           </button>
 //           `
 // }
+
+
+
+async function onButtonClick() {
+  searchQuery.page += 1;
+
+  const response = await searchQuery.searchPictures();
+  if (searchQuery.page > response.totalHits / searchQuery.per_page) {
+      refs.buttonPage.classList.add('visually-hidden');
+      Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+  }
+  refs.conteinerImg.insertAdjacentHTML('beforeend', markup(response.hits));
+
+  
+};
